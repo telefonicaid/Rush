f120a35a4c3d88e7fda7f892bddcdf34736708e0
Carlos Romero Brox <brox@tid.es>
2012-02-03 08:35:29 +0100

    retry policy

--- worker_relay.js
+++ worker_relay.js
@@ -2,6 +2,7 @@ var redis = require("redis");
 var http = require("http");
 var MG = require("./my_globals").C;
 var rcli = redis.createClient(redis.DEFAULT_PORT, '10.95.8.182');
+
 function do_job(task) {
     var target_host = task.headers[MG.HEAD_RELAYER_HOST];
     if (!target_host) {
@@ -17,10 +18,12 @@ function do_job(task) {
             path:task.url,
             headers:task.headers
         };
+        console.log("Haciendo peticion");
         console.dir(options);
         req = http.request(options, function (rly_res) {
             get_response(rly_res, task, function (task, resp_obj) {
-
+                console.log("resp from target");
+                console.log(resp_obj);
                 //PERSISTENCE
                 if (task.headers[MG.HEAD_RELAYER_PERSISTENCE]) {
                     set_redis(task, resp_obj, task.headers[MG.HEAD_RELAYER_PERSISTENCE]);
@@ -30,11 +33,16 @@ function do_job(task) {
             });
         });
         req.on('error', function (e) {
-            console.log('problem with request: ' + e.message);
+            console.log('problem with request: ' + e);
+            handle_error(e, task);
+
         });
         req.end();
     }
 }
+
+
+
 function get_response(resp, task, callback) {
     var data = "";
     resp.on('data', function (chunk) {
@@ -45,12 +53,17 @@ function get_response(resp, task, callback) {
             data += chunk;
         }
         var headers_str = JSON.stringify(resp.headers);
+        console.log("resp in get_response");
+        console.dir(resp);
         var resp_obj = {statusCode:resp.statusCode, headers:headers_str, body:data };
+
         if (callback) {
             callback(task, resp_obj);
         }
     });
 }
+
+
 function set_redis(task, resp_obj, type) {
     //TODO refactorize
     //remove from response what is not needed
@@ -82,6 +95,37 @@ function set_redis(task, resp_obj, type) {
                         console.log(red_res);
                     }
                 });
+}
+
+
+function handle_error(error, task, callback) {
+    if(task.headers[MG.HEAD_RELAYER_RETRY])
+        retry(task, callback);
+}
+
+function retry(task, callback) {
+    var retry_list =task.headers[MG.HEAD_RELAYER_RETRY];
+    var time = -1;
+
+    if(retry_list) {
+        retry_a = retry_list.split(",");
+        if(retry_a.length>0) {
+            time = parseInt(retry_a.shift(),10);
+            if (retry_a.length >0) {
+                // there is retry times still
+                task.headers[MG.HEAD_RELAYER_RETRY] = retry_a.join(",");
+            }
+            else {
+                delete task.headers[MG.HEAD_RELAYER_RETRY];
+            }
+            if (time > 0) {
+                setTimeout(function() {
+                        do_job(task);
+                    }, time);
+            }
+        }
+    }
+    if(callback) callback();
 }
 
 exports.do_job = do_job;
