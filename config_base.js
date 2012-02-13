e9ed0f81ac79957788199a595b57d58f43a42066
Carlos Romero Brox <brox@tid.es>
2012-02-13 11:57:24 +0100

    expire_time into dbrelayer section

--- config_base.js
+++ config_base.js
@@ -3,7 +3,7 @@
 exports.queue= {};
 exports.queue.redis_host = '10.95.8.182';
 
-exports.dbrelayer = {}
+exports.dbrelayer = {};
 exports.dbrelayer.key_prefix = 'wrH:';
 exports.dbrelayer.redis_host = '10.95.8.182';
-exports.expire_time = 60;
+exports.dbrelayer.expire_time = 60;
