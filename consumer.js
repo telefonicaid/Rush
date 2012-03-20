7f8351443193fac0f7078a145ec0132764c819c1
Marcos Reyes <mru@tid.es>
2012-03-20 16:42:15 +0100

    bug, adding clousure scope

--- consumer.js
+++ consumer.js
@@ -13,46 +13,45 @@ function consume(idconsumer, start) {
     else{
     store.get(obsQueues, idconsumer,processing_consumed_task);
     }
+
+    function processing_consumed_task(err, resp) {
+        if (err) {
+            logger.error("ERROR_________________", err);
+        }
+        else if(resp){
+            logger.info("resp", resp);
+            //EMIT PROCESSING
+            if (resp.queueId !== obsQueues.control) {
+                do_job = service_router.getWorker(resp);
+                do_job(resp.task, function onJobEnd(dojoberr){
+                    if (dojoberr){
+                        logger.error("ERROR_________________", dojoberr);
+                        //EMIT ERROR
+                    }
+                    else{
+                        //EMIT COMPLETED
+                    }
+                    store.rem_processing_queue(idconsumer, function onRemoval(err){
+                        if(err){
+                            logger.error("ERROR_________________", err);
+                        }
+                        else{
+                            process.nextTick(function consumer_clousure(){consume(idconsumer, false)});
+                        }
+                    });
+
+                });
+            }
+            else {  /* control */
+
+            }
+        }
+        else{
+            process.nextTick(function consumer_clousure(){consume(idconsumer, false)});
+        }
+    }
 }
 
 for (var i=0;i<max_poppers;i++){
       consume(C.consumer_id+i, true);
-}
-
-
-function processing_consumed_task(err, resp) {
-    if (err) {
-        logger.error("ERROR_________________", err);
-    }
-    else if(resp){
-        logger.info("resp", resp);
-        //EMIT PROCESSING
-        if (resp.queueId !== obsQueues.control) {
-            do_job = service_router.getWorker(resp);
-            do_job(resp.task, function onJobEnd(dojoberr){
-                if (dojoberr){
-                    logger.error("ERROR_________________", dojoberr);
-                    //EMIT ERROR
-                }
-                else{
-                    //EMIT COMPLETED
-                }
-                store.rem_processing_queue(idconsumer, function onRemoval(err){
-                    if(err){
-                        logger.error("ERROR_________________", err);
-                    }
-                    else{
-                        process.nextTick(function consumer_clousure(){consume(idconsumer, false)});
-                    }
-                });
-
-            });
-        }
-        else {  /* control */
-
-        }
-    }
-    else{
-        process.nextTick(function consumer_clousure(){consume(idconsumer, false)});
-    }
 }
