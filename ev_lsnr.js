e258c99cf523765803347e2fae0db88c9f44b7d6
Marcos Reyes <mru@tid.es>
2012-05-31 17:00:31 +0200

    Addin try / err handling ev_lsnr (Mongo failure)

--- ev_lsnr.js
+++ ev_lsnr.js
@@ -13,65 +13,77 @@ var config = require('./config_base').ev_lsnr;
 var clients = [];
 
 function init(emitter, callback) {
-    "use strict";
+  "use strict";
 
-    var client = new mongodb.Db(config.mongo_db, new mongodb.Server(config.mongo_host, config.mongo_port, {}));
+  var client = new mongodb.Db(config.mongo_db,
+    new mongodb.Server(config.mongo_host, config.mongo_port, {}));
 
-    client.open(function (err, p_client) {
-        client.collection(config.collectionState, function (err, c) {
-            if (err) {
-                if (callback) {
-                    callback(err);
-                }
-            }
-            else {
-                var collection = c;
-                emitter.on(G.EVENT_NEWSTATE, function new_event(data) {
-                    console.log("lNEW STATE ARRIVED");
-                    console.dir(data);
-                    collection.insert(data, function (err, docs) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            console.log(docs);
-                        }
-                    });
-                });
-                if (callback) {
-                    callback(null);
-                }
-            }
-        });
-
-      client.collection(config.collectionError, function (err, c) {
+  client.open(function(err, p_client) {
+    if (err) {
+      if (callback) {
+        callback(err);
+      }
+    } else {
+      client.collection(config.collectionState, function(err, c) {
         if (err) {
-            if (callback) {
-                callback(err);
-            }
-        }
-        else {
+          if (callback) {
+            callback(err);
+          }
+        } else {
           var collection = c;
-          emitter.on(G.EVENT_ERR, function new_error(data) {
-            console.log("lNEW ERROR ARRIVED");
-            console.dir(data);
-            collection.insert(data, function (err, docs) {
+          emitter.on(G.EVENT_NEWSTATE, function new_event(data) {
+            try {
+              console.log("lNEW STATE ARRIVED");
+              console.dir(data);
+              collection.insert(data, function(err, docs) {
                 if (err) {
-                    console.log(err);
+                  console.log(err);
+                } else {
+                  console.log(docs);
                 }
-                else {
-                    console.log(docs);
-                }
-            });
-          });
-            if (callback) {
-                callback(null);
+              });
+            } catch (e) {
+              console.log(e);
             }
+          });
+          if (callback) {
+            callback(null);
+          }
         }
       });
-    });
 
-    clients.push(client);
+      client.collection(config.collectionError, function(err, c) {
+        try {
+          if (err) {
+            if (callback) {
+              callback(err);
+            }
+          } else {
+            var collection = c;
+            emitter.on(G.EVENT_ERR, function new_error(data) {
+              console.log("lNEW ERROR ARRIVED");
+              console.dir(data);
+
+              collection.insert(data, function(err, docs) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log(docs);
+                }
+              });
+
+            });
+            if (callback) {
+              callback(null);
+            }
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      });
+      clients.push(client);
+    }
+  });
 }
 
 exports.init = init;
