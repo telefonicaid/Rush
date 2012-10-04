a1b754e8e219abce9ca32067c04b90d3c6eab856
brox <brox@tid.ers>
2012-10-04 10:33:25 +0200

    removed https

--- simple_server.js
+++ simple_server.js
@@ -17,18 +17,3 @@ var srv = http.createServer(function (req, res) {
 // now that server is running
 srv.listen(8124);
 
-// Create an HTTP server
-var srvs = https.createServer(function (req, res) {
-    req.on('data', function (data) {
-        console.log(""+data);
-    });
-    req.on('end', function() {
-        setTimeout( function() {
-            res.writeHead(203, {'Content-Type': 'text/plain'});
-            res.end('okay');
-        }, 2*1000);
-    });
-});
-
-// now that server is running
-srvs.listen(8125);
