8b08b9a2063c44d670d4e1f7f0663ae2afcd3042
Carlos Romero Brox <brox@tid.es>
2012-10-17 11:40:02 +0200

    chained susbcriptions to mongo collections

--- src/ev_lsnr.js
+++ src/ev_lsnr.js
@@ -16,7 +16,6 @@ var logger = log.newLogger();
 logger.prefix = path.basename(module.filename,'.js');
 
 
-var clients = [];
 
 function init(emitter, callback) {
   "use strict";
@@ -24,72 +23,85 @@ function init(emitter, callback) {
   var client = new mongodb.Db(config.mongo_db,
     new mongodb.Server(config.mongo_host, config.mongo_port, {}));
 
-  client.open(function(err, p_client) {
+    function subscribeStateCol(callback) {
+        client.collection(config.collectionState, function (err, c) {
+            if (err) {
+                logger.warning('collection', err);
+                if (callback) {
+                    callback(err);
+                }
+            } else {
+                var collection = c;
+                emitter.on(G.EVENT_NEWSTATE, function new_event(data) {
+                    try {
+                        logger.debug('new_event', data);
+                        collection.insert(data, function (err, docs) {
+                            if (err) {
+                                logger.warning('insert', err);
+                            } else {
+                                logger.debug('insert', docs);
+                            }
+                        });
+                    } catch (e) {
+                        logger.warning('new_event', e);
+                    }
+                });
+                if (callback) {
+                    callback(null);
+                }
+            }
+        });
+    }
+
+    function subscribeErrorCol(callback) {
+        client.collection(config.collectionError, function (err, c) {
+
+            if (err) {
+                logger.warning('collectionError', err);
+                if (callback) {
+                    callback(err);
+                }
+            } else {
+                var collection = c;
+                emitter.on(G.EVENT_ERR, function new_error(data) {
+                    try {
+                        logger.debug('new_error', data);
+
+                        collection.insert(data, function (err, docs) {
+                            if (err) {
+                                logger.warning('insert', err);
+                            } else {
+                                logger.debug('insert', docs);
+                            }
+                        });
+                    } catch (e) {
+                        logger.warning('new_error', e);
+                    }
+                });
+                if (callback) {
+                    callback(null);
+                }
+            }
+
+        });
+    }
+
+    client.open(function(err, p_client) {
     if (err) {
       logger.warning('open', err);
       if (callback) {
         callback(err);
       }
     } else {
-      client.collection(config.collectionState, function(err, c) {
-        if (err) {
-            logger.warning('collection', err);
-            if (callback) {
-            callback(err);
-          }
-        } else {
-          var collection = c;
-          emitter.on(G.EVENT_NEWSTATE, function new_event(data) {
-            try {
-              logger.debug('new_event', data);
-              collection.insert(data, function(err, docs) {
-                if (err) {
-                    logger.warning('insert', err);
-                } else {
-                  logger.debug('insert', docs);
-                }
-              });
-            } catch (e) {
-                logger.warning('new_event', e);
-            }
-          });
-          if (callback) {
-            callback(null);
-          }
-        }
-      });
-
-      client.collection(config.collectionError, function(err, c) {
-
-          if (err) {
-            logger.warning('collectionError', err);
-            if (callback) {
+      subscribeStateCol(function(err) {
+          if(err) {
               callback(err);
-            }
-          } else {
-            var collection = c;
-            emitter.on(G.EVENT_ERR, function new_error(data) {
-                try {
-                logger.debug('new_error', data);
-
-              collection.insert(data, function(err, docs) {
-                if (err) {
-                    logger.warning('insert', err);
-                } else {
-                    logger.debug('insert', docs);
-                }
-              });
-                } catch (e) {
-                    logger.warning('new_error', e);
-                }
-            });
-            if (callback) {
-              callback(null);
-            }
           }
-
+          else {
+          subscribeErrorCol(callback); 
+          }
       });
-      clients.push(client);
+
     }
   });
 }
