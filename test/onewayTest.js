e4e314de799fa5c2174f5046b3064dcfe84b1422
david <david@martell.(none)>
2012-10-17 09:35:39 +0200

    Test for oneway policy updated

--- test/onewayTest.js
+++ test/onewayTest.js
@@ -9,7 +9,7 @@
 var should = require('should');
 var config = require('./config.js');
 var http = require('http');
-var server = require('./simplyServer.js');
+var server = require('./simpleServer.js');
 var utils = require('./utils.js');
 
 var host = config.hostname;
@@ -17,34 +17,114 @@ var port = config.port;
 
 describe('Oneway', function () {
 
+    describe('#GET', function () {
+        it('Should return the same headers and the same method', function (done) {
+            var headers = {
+                'X-Relayer-Host':'http://localhost:8014'
+            }
 
-    it('Should return the id and the same body', function (done) {
-        var content = {
-            content: 'hola mundo'
-        }
+            var options = {
+                host:host,
+                port:port,
+                method:'GET',
+                headers:headers
+            }
 
-        var headers = {
-            'content-type': 'application/json',
-            'X-Relayer-Host': 'http://localhost:8014',
-            'X-relayer-persistence': 'BODY'
-        }
+            server.serverListener(function () {
+                utils.makeRequest(options, undefined, function () {
+                });
+            }, function (method, headers, body) {
+                method.should.equal('GET');
+                JSON.stringify(headers).should.include(JSON.stringify(options.headers['X-Relayer-Host']));
+                done();
+            })
+        });
+    });
 
-        var options = {
-            host: host,
-            port: port,
-            method:'POST',
-            headers: headers
-        }
+    describe('#POST', function () {
+        it('Should return the same headers, method and body', function (done) {
+            var content = {
+                content:'Hello World'
+            }
 
-        server.serverListener(function () {
-            utils.postObj(options, JSON.stringify(content), function () {
+            var headers = {
+                'content-type':'application/json',
+                'X-Relayer-Host':'http://localhost:8014'
+            }
+
+            var options = {
+                host:host,
+                port:port,
+                method:'POST',
+                headers:headers
+            }
+
+            server.serverListener(function () {
+                utils.makeRequest(options, JSON.stringify(content), function () {
+
+                });
+            }, function (method, headers, body) {
+                method.should.equal('POST');
+                JSON.stringify(headers).should.include(JSON.stringify(options.headers['content-type']));
+                JSON.stringify(headers).should.include(JSON.stringify(options.headers['X-Relayer-Host']));
+                JSON.parse(body).content.should.include(content['content']);
+                done();
             });
-        }, function (headers, body) {
-            JSON.stringify(headers).should.include(JSON.stringify(options.headers['content-type']));
-            JSON.stringify(headers).should.include(JSON.stringify(options.headers['X-Relayer-Host']));
-            JSON.stringify(headers).should.include(JSON.stringify(options.headers['X-relayer-persistence']));
-            JSON.parse(body).content.should.include(content['content']);
-            done();
+        });
+    });
+
+    describe('#PUT', function () {
+        it('Should return the same headers, method and body', function (done) {
+            var content = {
+                content:'Hello World'
+            }
+
+            var headers = {
+                'content-type':'application/json',
+                'X-Relayer-Host':'http://localhost:8014'
+            }
+
+            var options = {
+                host:host,
+                port:port,
+                method:'PUT',
+                headers:headers
+            }
+
+            server.serverListener(function () {
+                utils.makeRequest(options, JSON.stringify(content), function () {
+                });
+            }, function (method, headers, body) {
+                method.should.equal('PUT');
+                JSON.stringify(headers).should.include(JSON.stringify(options.headers['content-type']));
+                JSON.stringify(headers).should.include(JSON.stringify(options.headers['X-Relayer-Host']));
+                JSON.parse(body).content.should.include(content['content']);
+                done();
+            });
+        });
+    });
+
+    describe('#DELETE', function () {
+        it('Should return the same headers and the same method', function (done) {
+            var headers = {
+                'X-Relayer-Host':'http://localhost:8014'
+            }
+
+            var options = {
+                host:host,
+                port:port,
+                method:'DELETE',
+                headers:headers
+            }
+
+            server.serverListener(function () {
+                utils.makeRequest(options, undefined, function () {
+                });
+            }, function (method, headers, body) {
+                method.should.equal('DELETE');
+                JSON.stringify(headers).should.include(JSON.stringify(options.headers['X-Relayer-Host']));
+                done();
+            })
         });
     });
 });
