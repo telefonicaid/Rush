f5d86a10a8e89bcf7b6db4bc95b64e99433010f4
oelmaallem <oelmaallem@Greyjoy.(none)>
2012-10-19 10:30:59 +0200

    new Request Test

--- test/requestTest.js
+++ test/requestTest.js
@@ -18,7 +18,6 @@ describe('Request Test',function(){
     it('Should return protocol error(test 1)',function(done){
         options.headers['X-Relayer-Host'] = 'localhost:3001';
         utils.makeRequest(options,'Protocol error test', function(e,data){
-            console.log(data);
             JSON.parse(data).errors[0].should.be.equal('Invalid protocol localhost:3001');
             done();
         });
@@ -28,28 +27,34 @@ describe('Request Test',function(){
     it('Should return protocol error (test 2)',function(done){
         options.headers['X-Relayer-Host'] = 'no protocol';
         utils.makeRequest(options,'Protocol error test', function(e,data){
-            console.log(data);
             JSON.parse(data).errors[0].should.be.equal('Invalid protocol no protocol');
             done();
         });
     });
 
 
-    it('Should return protocol error (test 3)',function(done){
+    it('Should return invalid host error (test 3)',function(done){
         options.headers['X-Relayer-Host'] = 'http://';
-        utils.makeRequest(options,'Protocol error test', function(e,data){
-            console.log(data);
-            JSON.parse(data).errors[0].should.be.equal('Invalid protocol http://');
+        utils.makeRequest(options,'host error test', function(e,data){
+            JSON.parse(data).errors[0].should.be.equal('Host not specify');
             done();
         });
     });
 
 
-    it('Should return protocol error (test 4)',function(done){
+    it('Should return invalid host error (test 4)',function(done){
         options.headers['X-Relayer-Host'] = 'https://';
-        utils.makeRequest(options,'Protocol error test', function(e,data){
-            console.log(data);
-            JSON.parse(data).errors[0].should.be.equal('Invalid protocol https://');
+        utils.makeRequest(options,'host error test', function(e,data){
+            JSON.parse(data).errors[0].should.be.equal('Host not specify');
+            done();
+        });
+    });
+
+
+    it('Should return X-Relayer-Host missing error',function(done){
+        delete options.headers['X-Relayer-Host'];
+        utils.makeRequest(options,'X-Relayer-Host missing test', function(e,data){
+             JSON.parse(data).errors[0].should.be.equal('x-relayer-host is missing');
             done();
         });
     });
