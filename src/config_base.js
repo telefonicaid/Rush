606f4edbcbdc7ca6aa34650dd650a8c77dac52d3
Carlos Romero Brox <brox@tid.es>
2012-10-26 12:09:49 +0200

    generic ev listener loaded at startup

--- src/config_base.js
+++ src/config_base.js
@@ -54,7 +54,8 @@ exports.listener.port = 3001;
 exports.listener.evModules = ['./ev_lsnr'];
 
 exports.consumer = {};
-exports.consumer.evModules = ['./ev_lsnr', './ev_callback', './ev_persistence'];
+exports.consumer.evModules = ['./ev_lsnr', './ev_callback', './ev_persistence','./gevlsnr'];
+
 exports.consumer.max_poppers = 500;
 // agent: undefined -> globalAgent | false -> no agent
 exports.consumer.agent = undefined;
