c60598d86f4ff28fc04a5e33afe143e05ae122f4
david <david@martell.(none)>
2012-12-03 13:01:24 +0100

    client now launches server for benchmark testing

--- benchmark/client.js
+++ benchmark/client.js
@@ -1,30 +1,32 @@
 var http = require('http');
-var request = 'test';
+var server = require('./server.js');
 
 var client = function (rushHost, rushPort, hostAndPort, timeout, resSize) {
     'use strict';
 
-    var options = {};
-    options.host = rushHost;
-    options.port = rushPort;
-    options.headers = {};
-    options.headers['X-Relayer-Host'] = hostAndPort;
-    //options.headers['X-relayer-persistence'] = 'BODY';
-    options.method = 'POST';
+    var createServer = server.createServer(timeout, resSize, function () {
+        var options = {};
+        options.host = rushHost;
+        options.port = rushPort;
+        options.headers = {};
+        options.headers['X-Relayer-Host'] = hostAndPort;
+        //options.headers['X-relayer-persistence'] = 'BODY';
+        options.method = 'POST';
 
-    var req = http.request(options, function (res) {
+        var req = http.request(options, function (res) {
 
-        res.on('data', function (chunk) {
-            var data = '';
-            data += chunk;
-            console.log(data);
+            res.on('data', function (chunk) {
+                var data = '';
+                data += chunk;
+                console.log(data);
+            });
         });
-    });
 
-    var body = { timeout : timeout, resSize : resSize};
-    body = JSON.stringify(body);
-    req.write(body);
-    req.end();
-}
+        var body = { timeout:timeout, resSize:resSize};
+        body = JSON.stringify(body);
+        req.write(body);
+        req.end();
+    });
+};
 
 exports.client = client;
