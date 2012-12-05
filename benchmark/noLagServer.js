a8fbd264b514602bf6290bcf1caced2b50437772
david <david@martell.(none)>
2012-12-05 10:47:35 +0100

    server port changed to 5001 and testing with a server with no lag added

--- benchmark/noLagServer.js
+++ benchmark/noLagServer.js
@@ -3,10 +3,8 @@ var qm = require('./queueMonitor.js');
 var client = require('./client.js');
 var server = require('./server.js');
 
-
-
-var scenario1 = pf.describe('First scenario', 'In this scenario we will test Rush behaviour with a server with ' +
-    'not much lag and a short response', 'wijmo', ['Xaxis', 'Yaxis'], [], './log'); //monitor on localhost
+var scenario1 = pf.describe('Second scenario', 'In this scenario we will test Rush behaviour with a server wit no lag'
+    , 'wijmo', ['Xaxis', 'Yaxis'], [], './log'); //monitor on localhost
 scenario1.test('test1', function (log, point) {
     'use strict';
     var i = 0;
@@ -18,14 +16,14 @@ scenario1.test('test1', function (log, point) {
         i++;
     }, 1000);
 
-    var server1 = server.createServer(5000, 1000, function () {
+    var server1 = server.createServer(0, 1000, function () {
         var completed = 0;
         function doReq(i){
             client.client('localhost', 3001, "http://localhost:5001");
             i++;
             if(i < 300){
                 setTimeout(function(){
-                doReq(i);
+                    doReq(i);
                 },10);
             }
 
