af245cf43da09e0d1befc4acdab7c2538908ae22
xval <xval@hi-iberia.es>
2013-06-10 12:05:54 +0300

    Add makerequests with Https

--- test/e2e/utils.js
+++ test/e2e/utils.js
@@ -1,4 +1,5 @@
 http = require('http');
+https = require('https');
 
 var makeRequest = function(options, content, cb) {
   'use strict';
@@ -29,4 +30,35 @@ var makeRequest = function(options, content, cb) {
 
 };
 
+var makeRequestHttps = function(options, content, cb) {
+    'use strict';
+    options.agent = new https.Agent(options);
+
+    var req = https.request(options, function(res) {
+       // console.log("statusCode: ", res.statusCode);
+       // console.log("headers: ", res.headers);
+        var data = ''; //returned object from request
+        res.setEncoding('utf8');
+
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
+
+        res.on('end', function() {
+            cb(null, data);
+        });
+    });
+
+    req.on('error', function(e) {
+        cb(e, null);
+    });
+
+    if (options.method === 'POST' || options.method === 'PUT') {
+        req.write(content);
+    }
+
+    req.end();
+};
+
 exports.makeRequest = makeRequest;
+exports.makeRequestHttps = makeRequestHttps;
