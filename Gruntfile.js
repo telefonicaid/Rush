a7a2a8bf2d5891967f13f34cb489cea89ccd6e66
XavierVal <sr.valcanto@gmail.com>
2013-06-11 10:53:30 +0200

    updated Gruntfile e2e test group

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
                 ui: 'bdd',
                 reporter: 'spec'
             },
-            e2e: {
+            acceptance: {
                 src: [
                     'tools/mocha-globals.js',
                     'test/acceptance/*.js'
