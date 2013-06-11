7f7832e1f4c12ac71f74c4667cb92d8cce17b6d0
XavierVal <sr.valcanto@gmail.com>
2013-06-11 10:48:27 +0200

    updated new tests paths

---
+++ test/acceptance/onewayTest.js
@@ -0,0 +1,81 @@
+var http = require('http');
+var should = require('should');
+var config = require('./config.js');
+var server = require('./simpleServer.js');
+var utils = require('./utils.js');
+
+var HOST = config.rushServer.hostname;
+var PORT = config.rushServer.port;
+
+var HEADER_TEST_VALUE = 'valueTest';
+
+var serversToShutDown = [];
+
+function executeTest(method, content, done) {
+  var headers = {
+    'X-Relayer-Host': 'http://localhost:8014',
+    'testheader': HEADER_TEST_VALUE
+  };
+
+  var options = {
+    host: HOST,
+    port: PORT,
+    method: method,
+    headers: headers
+  };
+
+  var simpleServer = server.serverListener(
+      function() {
+        utils.makeRequest(options, content, function() {
+        });
+      },
+      function(method, headers, body) {
+        method.should.equal(method);
+
+        headers.should.have.property('testheader',
+            HEADER_TEST_VALUE);
+
+        if (content) {
+
+          body.should.be.equal(content);
+        }
+
+        done();
+      }
+  );
+
+  serversToShutDown.push(simpleServer);
+}
+
+describe('Oneway', function() {
+
+  afterEach(function() {
+    for (var i = 0; i < serversToShutDown.length; i++) {
+      try {
+        serversToShutDown[i].close();
+      } catch (e) {
+
+      }
+    }
+
+    serversToShutDown = [];
+  });
+
+  it('Should return the same headers and the same method (GET)', function(done) {
+    executeTest('GET', undefined, done);
+  });
+
+  it('Should return the same headers, method and body (POST)', function(done) {
+    var content = 'Hello World'
+    executeTest('POST', content, done);
+  });
+
+  it('Should return the same headers, method and body (PUT)', function(done) {
+    var content = 'Hello World'
+    executeTest('PUT', content, done);
+  });
+
+  it('Should return the same headers and the same method (DELETE)', function(done) {
+    executeTest('GET', undefined, done);
+  });
+});
---
+++ test/acceptance/persistence.js
@@ -0,0 +1,180 @@
+var async = require('async');
+var should = require('should');
+var config = require('./config.js')
+var server = require('./simpleServer.js');
+var utils = require('./utils.js');
+
+var serversToShutDown = [];
+
+var HOST = config.rushServer.hostname;
+var PORT = config.rushServer.port;
+
+function executeTest(method, content, persistence, done) {
+  'use strict';
+
+  var id, options = {};
+  options.host = HOST;
+  options.port = PORT;
+  options.headers = {};
+  options.method = method;
+  options.headers['content-type'] = 'application/json';
+  options.headers['X-Relayer-Host'] = 'http://localhost:8014';
+  options.headers['X-relayer-persistence'] = persistence;
+  options.headers['test-header'] = 'test header';
+
+
+  var simpleServer = server.serverListener(
+
+      function() {
+        utils.makeRequest(options, content, function(err, res) {
+          id = JSON.parse(res).id;
+        });
+      },
+
+      function(method, headers, body) {
+
+        var checked = false;
+        var interval = setInterval(function() {
+
+          var options = { port: PORT, host: HOST,
+            path: '/response/' + id, method: 'GET'};
+
+          function checkResponse(err, data) {
+
+            if (data !== '{}' && ! checked) {
+
+              clearInterval(interval);
+
+              var JSONres = JSON.parse(data);
+
+              if (persistence === 'BODY') {
+
+                JSONres.should.have.property('body');
+                JSONres.body.should.be.equal(content);
+                JSONres.headers.should.have.property('test-header',
+                    'test header');
+                JSONres.should.have.property('statusCode', '200');
+
+              } else if (persistence === 'HEADER') {
+
+
+                JSONres.should.not.have.property('body');
+                JSONres.should.have.property('headers');
+                JSONres.headers.should.have.property('test-header',
+                    'test header');
+                JSONres.should.have.property('statusCode', '200');
+
+              } else if (persistence === 'STATUS') {
+
+                JSONres.should.not.have.property('body');
+                JSONres.should.not.have.property('headers');
+                JSONres.should.have.property('statusCode', '200');
+
+              }
+
+              checked = true;
+              done();
+
+            }
+          }
+
+          utils.makeRequest(options, '', checkResponse);
+
+        }, 10);
+      }
+  );
+
+  serversToShutDown.push(simpleServer);
+}
+
+describe('Persistence', function() {
+
+  afterEach(function() {
+    for (var i = 0; i < serversToShutDown.length; i++) {
+      try {
+        serversToShutDown[i].close();
+      } catch (e) {
+
+      }
+    }
+
+    serversToShutDown = [];
+  });
+
+
+
+  it('should return empty body and test-header', function(done) {
+    executeTest('GET', '', 'BODY', done);
+  });
+
+  it('should return the same body', function(done) {
+    executeTest('POST', 'Body Example', 'BODY', done);
+  });
+
+  it ('should return status and header. should not return body', function(done) {
+    executeTest('POST', 'Body Example', 'HEADER', done);
+  });
+
+  it ('should not return body neither heraders', function(done) {
+    executeTest('POST', 'Body Example', 'STATUS', done);
+  });
+
+  it('should return error headers (ENOTFOUND)', function(done) {
+    var id, options = {};
+    options.host = HOST;
+    options.port = PORT;
+    options.headers = {};
+    options.method = 'POST';
+    options.headers['X-Relayer-Host'] = 'http://notAServer:8014';
+    options.headers['X-relayer-persistence'] = 'BODY';
+    options.headers['test-header'] = 'test header';
+
+    async.series([
+      function(callback) {
+        utils.makeRequest(options, 'body request', function(err, res) {
+          id = JSON.parse(res).id;
+
+          if (!err) {
+            callback(null, res);
+          } else {
+            callback(err, null);
+          }
+
+        });
+      },
+
+      function(callback) {
+
+        var checked = false;
+        var interval = setInterval(function() {
+
+          var options = { port: PORT, host: HOST,
+            path: '/response/' + id, method: 'GET'};
+
+          function checkResponse(err, data) {
+
+            if (data !== '{}' && ! checked) {
+
+              clearInterval(interval);
+
+              if (!err) {
+                callback(null, JSON.parse(data));
+              } else {
+                callback(err, null);
+              }
+
+              checked = true;
+            }
+          }
+
+          utils.makeRequest(options, '', checkResponse);
+
+        }, 10);
+      }
+    ], function(err, res) {
+      var resGet = res[1];
+      resGet.should.have.property('error', 'ENOTFOUND(getaddrinfo)');
+      done();
+    });
+  });
+});
