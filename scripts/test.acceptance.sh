e059d6c973dadf8cd987d9a4ce9d85ef8ed78aea
XavierVal <sr.valcanto@gmail.com>
2013-06-13 13:59:51 +0200

    new acceptance criteria for standard scripts and cfg

--- scripts/test.acceptance.sh
+++ scripts/test.acceptance.sh
@@ -1,56 +1,20 @@
+############################ Config file version#######################################
+# To run script edit acceptance.cfg and type ./test.acceptance.sh or direct use ./test.acceptance.sh test_suite environment to override config
+
 #!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-# Launch this script from the base path of Rush
-echo "// Test Environmnet setup"
-SERVICES="mongod redis-server"
-RUSH="consumer.js listener.js"
+# sh file to execute the files based on configuration file
 
+. ../config/test.acceptance.cfg
 
-# check if each service is running
-for service in $SERVICES
-do
-  # if current service is running, dont do anything.
-  if ps ax | grep -v grep | grep $service > /dev/null
-  then
-    echo "$service service is running!"
-  else # otherwise, turn it on
-    echo "$service is not running"
-    sudo /etc/init.d/$service start
-    if ps ax | grep -v grep | grep $service > /dev/null
-    then
-      echo "$service is now running."
-    else
-      echo "Unable to start $service."
-    fi
-  fi
-done
+rm -rf test/acceptance/testReport/*.xml
+cd ../test/acceptance
+mkdir -p logs
 
-#check Rush is running with at least one agent of consumer and listener
-for rush in $RUSH
-do
-  # if current service is running, dont do anything.
-  if ps ax | grep -v grep | grep $rush > /dev/null
-  then
-    echo "$rush service is running!"
-  else # otherwise, turn it on
-    echo "$rush is not running"
-     node lib/$rush &
-    if ps ax | grep -v grep | grep $rush > /dev/null
-    then
-      echo "$rush is now running."
-    else
-      echo "Unable to start $rush."
-    fi
-  fi
-done
+if [ "$1" != "" ]&&[ "$2" != "" ]
+then
+./acceptance*.sh $1 $2./logs/acceptance.log 2>&1
+else
+./acceptance*.sh $test_suite $environment >./logs/acceptance.log
+fi
 
-# mongod
-# usr/sbin/mongod
-# redis-server
-# node lib/consumer.js &
-# node lib/listener.js &
-echo " // Test execution starting: "
-echo "+++++++++++++++++++++++++++++"
-echo " "
-grunt test
-exit
+exit $?
