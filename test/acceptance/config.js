6d79ec200ce3305847016cf774eb237f63ad025c
XavierVal <sr.valcanto@gmail.com>
2013-08-21 13:38:23 +0200

    acceptance config merge

--- test/acceptance/config.js
+++ test/acceptance/config.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
 exports.rushServer = {hostname: 'telefonicaspain-test.apigee.net/rush/v1', port: 443};
 exports.externalEndpoint = 'www.google.com';
 exports.token = 'jqv1vOp1qzscJ0NNh4sTn3l3AlEG';
->>>>>>> unstable
