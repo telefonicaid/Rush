83bcede7edb2e246cd04e63525974bf2a240bbf9
Fernando Godino González <fgodino@conwet.com>
2013-09-02 13:48:49 +0200

    test menu

--- scripts/test.alarms.sh
+++ scripts/test.alarms.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 CERT_PATH=../utils/server.crt
 KEY_PATH=../utils/server.key
@@ -11,38 +12,14 @@ RUSH_HOST=http://localhost:5001
 VERBOSE=false
 FORCE=false
 
-while getopts vf option; do
-    case "${option}"
-    in
-      v) VERBOSE=true;;
-      f) FORCE=true;;
-    esac
-  done
-
-echo $FORCE
-
-if $FORCE  ##Kill mongo and redis if there is some
-then
+function killall {
   killall redis-server
   killall mongod
   while nc -z localhost 6379 || nc -z localhost 27017;
     do
       sleep 1
     done
-
-else ##Stop if there is active servers otherwise
-  if nc -z localhost 6379; then
-    echo There is an active redis-server instance.
-    echo Stop it or use --force
-    exit 1
-  fi
-
-  if nc -z localhost 27017; then
-    echo There is an active mongod instance.
-    echo Stop it or run this script with flag -f
-    exit 1
-  fi
-fi
+}
 
 function before {
   rm -rf mongo
@@ -52,7 +29,7 @@ function before {
   redis_pid=$!
   mongod --dbpath ./mongo --quiet & #Start mongodb
   mongo_pid=$!
-  until nc -z localhost 27017 && nc -z localhost 6379 ##Wait until they both are up
+  until nc -z localhost 27017 && nc -z localhost 6379 ##Wait until they are both up
     do
       sleep 1
     done
@@ -90,6 +67,10 @@ function no_certs_found { #Throw no certs found error
   $LISTENER &
   afterEach
   cert_recover
+  grep -q -e '| lvl=WARNING | op=LISTENER START UP | msg=Certs Not Found |' $LOG
+  if [  $? -ne 0 ]; then
+    echo "Scenario 1: Should log Certs not found error"
+  fi
 }
 
 function invalid_request {
@@ -97,6 +78,11 @@ function invalid_request {
   beforeEach
   curl -v -H "X-Relayer-Host:nodejs.org" -H "X-relayer-persistence: INVALID_PERSISTENCE" $RUSH_HOST #nvalid persistence, should be logged
   afterEach
+  grep -q -e '| lvl=WARNING | op=ASSIGN REQUEST | msg=Request Error |' $LOG
+  if [  $? -ne 0 ]; then
+    echo "Scenario 2: Should log Request Error"
+    return_value=1
+  fi
 }
 
 function redis_unavailable {
@@ -104,6 +90,11 @@ function redis_unavailable {
   beforeEach
   $LISTENER &
   afterEach
+  grep -q -e '| lvl=ERROR | op=REDIS CONNECTION | msg=Redis Error |' $LOG
+  if [  $? -ne 0 ]; then
+    echo "Scenario 3: Should log Redis Error"
+    return_value=1
+  fi
   redis-server & #Restart redis
   redis_pid=$!
   until nc -z localhost 6379 ##Wait until it is up
@@ -123,48 +114,82 @@ function mongo_unavailable {
     do
       sleep 1
     done
+  grep -q -e '| lvl=WARNING | op=INIT EVENT LISTENER | msg=Could not connect with MongoDB |' $LOG
+  if [  $? -ne 0 ]; then
+    echo "Scenario 4: Should log Could not connect with MongoDB"
+    return_value=1
+  fi
+  grep -q -e '| lvl=ERROR | op=ADD-ONS START UP | msg=Error subscribing event listener |' $LOG
+  if [  $? -ne 0 ]; then
+    echo "Scenario 5: Should log Error subscribing event listener"
+    return_value=1
+  fi
 }
 
-before
 
-no_certs_found
-invalid_request
-redis_unavailable
-mongo_unavailable
+function menu {
+  before
+  case $option in
+        1)
+          no_certs_found
+          ;;
+        2)
+          invalid_request
+          ;;
+        3)
+          redis_unavailable
+          ;;
+        4)
+          mongo_unavailable
+          ;;
+        0)
+          no_certs_found
+          invalid_request
+          redis_unavailable
+          mongo_unavailable
+          ;;
+        *) echo "Invalid input"
+            ;;
+  esac
+  after
+}
 
-after
 
-return_value=0
+echo "----Alarm tests---"
+echo "Options:"
+echo "S: Stop redis-server and mongodb"
+echo "1: Run Scenario 1. HTTPS certs not found"
+echo "2: Run Scenario 2. Invalid request"
+echo "3: Run Scenario 3. Redis server unavailable"
+echo "4: Run Scenario 4. MongoDB unavailable"
+echo "0: Run ALL tests"
 
+echo -n "Choose one of the previous options: "
+read option
 
-##Check log file
+if [ $option == "S" ]
+then
+  killall
+else
+  if nc -z localhost 6379; then
+    echo There is an active redis-server instance.
+    echo Stop it or use --force
+    exit 1
+  fi
 
-grep -q -e '| lvl=WARNING | op=LISTENER START UP | msg=Certs Not Found |' $LOG
-if [  $? -ne 0 ]; then
-  echo "Scenario 1: Should log Certs not found error"
-  return_value=1
+  if nc -z localhost 27017; then
+    echo There is an active mongod instance.
+    echo Stop it or run this script with flag -f
+    exit 1
+  fi
+  menu
 fi
-grep -q -e '| lvl=WARNING | op=ASSIGN REQUEST | msg=Request Error |' $LOG
-if [  $? -ne 0 ]; then
-  echo "Scenario 2: Should log Request Error"
-  return_value=1
-fi
-grep -q -e '| lvl=ERROR | op=REDIS CONNECTION | msg=Redis Error |' $LOG
-if [  $? -ne 0 ]; then
-  echo "Scenario 3: Should log Redis Error"
-  return_value=1
-fi
-grep -q -e '| lvl=WARNING | op=INIT EVENT LISTENER | msg=Could not connect with MongoDB |' $LOG
-if [  $? -ne 0 ]; then
-  echo "Scenario 4: Should log Could not connect with MongoDB"
-  return_value=1
-fi
-grep -q -e '| lvl=ERROR | op=ADD-ONS START UP | msg=Error subscribing event listener |' $LOG
-if [  $? -ne 0 ]; then
-  echo "Scenario 5: Should log Error subscribing event listener"
-  return_value=1
-fi
+
+
+
 
 exit $return_value
+
+
 
 
