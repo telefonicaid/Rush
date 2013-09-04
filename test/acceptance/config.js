ad0769b3d5add27c8f8ba0f0275c770b0acc286a
XavierVal <sr.valcanto@gmail.com>
2013-09-04 12:29:21 +0200

    acceptance test fix: completed isn't finished task

--- test/acceptance/config.js
+++ test/acceptance/config.js
@@ -1,4 +1,3 @@
-exports.rushServer = {hostname: 'ec2-54-246-229-238.eu-west-1.compute.amazonaws.com', port: 443};
-exports.externalEndpoint = 'www.google.com';
-exports.user = 'rush:RuSh1';
+exports.rushServer = {hostname: '54.229.153.95', port: 80};
+exports.externalEndpoint = 'www.google.es';
 exports.token = '';
