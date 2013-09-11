9a9c50fb05d989890cfe7c320e61afdb50c2b2ac
Fernando Godino González <fgodino@conwet.com>
2013-09-11 10:41:49 +0200

    HA Test

--- test/e2e/haTest.js
+++ test/e2e/haTest.js
@@ -45,22 +45,51 @@ function _validScenario(data){
 	});
 }
 
+function _invalidScenario(data){
+	it.skip(data.name +  ' #FHA', function(done){
+		var agent = superagent.agent();
+		var id;
+    if (vm) {
+	    //Steps input
+    }
+
+	});
+}
+
+
 
 describe('Single Feature: High Availability '  + '#FHA', function() {
 	this.timeout(describeTimeout);
 
 		serversToShutDown = [];
-	});
+});
 
 
 
 
 	describe('Scenario HA AWS Standard [ 1LoadBalancer (LB) | 2Rush-Listeners(RL1,RL2) + 2Rush-Consumers(RC1,RC2) | 2Sentinel(S1,S2) + 2Redis (R1,R2) ]', function () {
-		describe(' Tolerance to errors', function () {
 
+		describe(' Tolerance to errors', function () {
 		var dataSetPOST = [
 			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
-					"1 Should accept requests when server X is down"},
+					"1 Should accept requests when S1 is down, delegating monitorization to Sentinel2"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"2 Should accept requests when S2 is down, Sentinel detects S2 as down"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"3 Should accept requests when S1 and S2 is down, HA monitorization is lost"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"4 Should accept requests when server R1 is down, some requests may be lost. Delegating storage to R2."},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"5 Should accept requests when server R2 is down, Sentinel detects R2 as down. No slaves to promote"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"7 Should accept requests when RL1 is down. Only one listener left"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"8 Should accept requests when RL2 is down. Only one listener left"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"9 Should accept requests when RC1 is down. Only one consumer left"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"10 Should accept requests when RC2 is down. Only one consumer left"}
+
 		];
 
 		for(i=0; i < dataSetPOST.length; i++){
@@ -72,16 +101,92 @@ describe('Single Feature: High Availability '  + '#FHA', function() {
 
 			var dataSetPOST = [
 				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
-						"1 Should NOT accept requests when X is down"},
+						"1 Should NOT accept requests when LB is down. Workers are still completing enqueued requests."},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"2 Should NOT accept requests when R1 and R2 are down. No storage servers left"},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"3 Should NOT accept requests when RL1 and RL2 are down. Workers are still completing enqueued requests."},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"4 Should NOT accept requests when RC1 and RC2 are down. Rush requests can be enqueued, but not served"},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"5 Should NOT accept requests when S1, S2, and R1 are down. No way to promote slaves"},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"6 Should NOT accept requests when S1, S2, and R2 are down. No way to promote slaves"}
 			];
 
 			for(i=0; i < dataSetPOST.length; i++){
-				_validScenario(dataSetPOST[i]);  //Launch every test in data set
+				_invalidScenario(dataSetPOST[i]);  //Launch every test in data set
 			}
 		});
 
 
 	});
+
+	describe(' Scenario HA AWS BIG [ 1LoadBalancerAWS  (AWSLB1)| 2LoadBalancer (LB1,LB2) | 2Rush-Listeners(RL1,RL2) + 2Rush-Consumers(RC1,RC2) | 2Sentinel(S1,S2) + 2Redis (R1,R2) ]', function () {
+
+		describe(' Tolerance to errors', function () {
+		var dataSetPOST = [
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"1 Should accept requests when S1 is down, delegating monitorization to Sentinel2"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"2 Should accept requests when S2 is down, Sentinel detects S2 as down"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"1 Should accept requests when S1 is down, delegating monitorization to Sentinel2"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"2 Should accept requests when S2 is down, Sentinel detects S2 as down"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"3 Should accept requests when S1 and S2 is down, HA monitorization is lost"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"4 Should accept requests when server R1 is down, some requests may be lost. Delegating storage to R2."},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"5 Should accept requests when server R2 is down, Sentinel detects R2 as down. No slaves to promote"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"7 Should accept requests when RL1 is down. Only one listener left"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"8 Should accept requests when RL2 is down. Only one listener left"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"9 Should accept requests when RC1 is down. Only one consumer left"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"10 Should accept requests when RC2 is down. Only one consumer left"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"11 Should accept requests when LB1 is down, AWSLB1 distributes ALL petitions to LB2"},
+			{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+					"12 Should accept requests when LB2 is down, AWSLB1 distributes ALL petitions to LB1"}
+		];
+
+		for(i=0; i < dataSetPOST.length; i++){
+			_validScenario(dataSetPOST[i]);  //Launch every test in data set
+		}
+		});
+
+		describe(' NO Tolerance to errors', function () {
+
+			var dataSetPOST = [
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"1 Should NOT accept requests when AWSLB1 is down. Workers are still completing enqueued requests."},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"2 Should NOT accept requests when LB1 and LB2 are down. Workers are still completing enqueued requests."},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"3 Should NOT accept requests when R1 and R2 are down. No storage servers left"},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"4 Should NOT accept requests when RL1 and RL2 are down. Workers are still completing enqueued requests."},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"5 Should NOT accept requests when RC1 and RC2 are down. Rush requests can be enqueued, but not served"},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"6 Should NOT accept requests when S1, S2, and R1 are down. No way to promote slaves"},
+				{protocol : 'http', method: 'GET', path: '/', headers: {}, body: {}, name :
+						"7 Should NOT accept requests when S1, S2, and R2 are down. No way to promote slaves"}
+			];
+
+			for(i=0; i < dataSetPOST.length; i++){
+				_invalidScenario(dataSetPOST[i]);  //Launch every test in data set
+			}
+		});
+
+
+	});
+
+
 
 
 
